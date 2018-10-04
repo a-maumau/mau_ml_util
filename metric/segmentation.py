e892f5cc74b8649c315dcb545122cc29785ebbdb
a-maumau <maumaumaumaumaumaumaumaumaumau@gmail.com>
2018-10-04 20:03:09 +0900

    fix

--- metric/segmentation.py
+++ metric/segmentation.py
@@ -7,6 +7,67 @@ import torch
 CPU = torch.device('cpu')
 
 class SegmentationMetric(object):
+    """
+        the matrix is holding the value in order
+                ground truth
+                0 1 2 3 4 ...
+        pred  0
+              1
+              2
+              3
+              4
+              .
+              .
+              .
+
+        so, in 3 class case with following tensor
+        pred_tensor = [
+                    [[0,1,1],
+                     [0,2,2],
+                     [1,1,2]],
+                    ]
+        gt_tensor = [
+                    [[1,1,1],
+                     [0,2,2],
+                     [0,0,2]],
+                    ]
+
+        the matrix is goiing to be
+               |ground truth
+               |0 1 2
+        -------+-----
+        pred 0 |1 1 0
+             1 |2 2 0
+             2 |0 0 3
+
+        and, to calc iou, for example,
+        class 0:
+            TP is 
+               |0 1 2
+            ---+-----
+             0 |1 * *
+             1 |* * *
+             2 |* * *
+
+            FP is 
+               |0 1 2
+            ---+-----
+             0 |* 1 0
+             1 |* * *
+             2 |* * *
+
+            FN is
+               |0 1 2
+            ---+-----
+             0 |* * *
+             1 |2 * *
+             2 |0 * *
+
+        so IoU of class 0 is TP/(TP+FP+FN) = 1/(1+1+2+) = 0.25
+
+        and so on.
+    """
+
     def __init__(self, class_num, map_device=CPU):
         self.class_num = class_num
         self.map_device = map_device
@@ -24,18 +85,62 @@ class SegmentationMetric(object):
         for gt_class_id in range(self.class_num):
             gt_class = torch.eq(gt_label, gt_class_id).to(dtype=torch.long)
 
-            print("top for")
-            print(gt_class)
-
             for pred_class_id in range(self.class_num):
                 pred_class = torch.eq(pred_label, pred_class_id).to(dtype=torch.long)
-                print("intra for")
-                print(pred_class)
                 pred_class = torch.mul(gt_class, pred_class)
-                print(pred_class)
                 count = torch.sum(pred_class)
-                print(count)
                 self.class_matrix[pred_class_id, gt_class_id] += count
+
+    def calc_pix_acc(hist_matrix):
+        return torch.trace(self.class_matrix)/torch.sum(self.class_matrix)
+
+    def calc_mean_pix_acc(self, ignore=[255]):
+        if isinstance(ignore, int):
+            ignore = [ignore]
+        mean_pix_acc = {}
+
+        for class_id in range(self.class_num):
+            if class_id in ignore:
+                continue
+
+            mean_pix_acc["class_{}".format(class_id)] = (self.class_matrix[class_id, class_id] /
+                                                         torch.sum(self.class_matrix[:, class_id])).cpu().item()
+
+        return mean_pix_acc
+
+    def calc_mean_jaccard_index(self, ignore=[255]):
+        """
+            it is same to IoU
+        """
+
+        if isinstance(ignore, int):
+            ignore = [ignore]
+        iou = {}
+
+        for class_id in range(self.class_num):
+            if class_id in ignore:
+                continue
+
+            iou["class_{}".format(class_id)] = (self.class_matrix[class_id, class_id] /
+                                               (torch.sum(self.class_matrix[class_id, :]) +
+                                                torch.sum(self.class_matrix[:, class_id]) -
+                                                self.class_matrix[class_id, class_id])).cpu().item()
+
+        return iou
+
+    def calc_mean_precision(self, ignore=[255]):
+        if isinstance(ignore, int):
+            ignore = [ignore]
+        precision = {}
+
+        for class_id in range(self.class_num):
+            if class_id in ignore:
+                continue
+
+            precision["class_{}".format(class_id)] = (self.class_matrix[class_id, class_id] /
+                                               (torch.sum(self.class_matrix[class_id, :])-self.class_matrix[class_id, class_id])).cpu().item()
+
+        return precision
 
 # test
 if __name__ == '__main__':
@@ -62,18 +167,24 @@ if __name__ == '__main__':
         batch1 j: class0=0.0, class1=0.0, class2=0.0
         mean   j: class0=0.125, class1=0.2, class2=0.5
     """
-    input_tensor = [
+    pred_tensor = [
+                    [[0,1,1],
+                     [0,2,2],
+                     [1,1,2]],
                     [[1,1,1],
                      [0,1,0],
                      [0,0,0]]
                     ]
     gt_tensor = [
+                    [[1,1,1],
+                     [0,2,2],
+                     [0,0,2]],
                     [[2,2,2],
                      [2,2,2],
                      [2,2,2]]
                 ]
 
-    p = torch.LongTensor(input_tensor).to(device=map_device)
+    p = torch.LongTensor(pred_tensor).to(device=map_device)
     g = torch.LongTensor(gt_tensor).to(device=map_device)
 
     print("prediction tensor")
@@ -84,8 +195,11 @@ if __name__ == '__main__':
     m = SegmentationMetric(3)
     m(p, g)
 
-    print(m.class_matrix)
-    print(m.l(p, g))
+    print(.calc_pix_acc)
+    print(m.calc_mean_pix_acc)
+    print(m.calc_mean_jaccard_index)
+    print(m.calc_mean_precision)
+
 
     """
     results = [
