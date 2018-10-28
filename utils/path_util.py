d0e486ce783974f5f11fa6f5e67ca6eedceb3498
a-maumau <maumaumaumaumaumaumaumaumaumau@gmail.com>
2018-10-29 00:22:49 +0900

    fix

--- utils/path_util.py
+++ utils/path_util.py
@@ -1,17 +1,17 @@
 import os
 
 def mkdir(folder_path):
-    if not os.path.exists(os.path.abspath(folder_path)):
-        os.makedirs(os.path.abspath(folder_path))
+    if not os.path.exists(os.path.expanduser(folder_path)):
+        os.makedirs(os.path.expanduser(folder_path))
 
 def is_dir(folder_path):
-	return os.path.isdir(os.path.abspath(folder_path))
+	return os.path.isdir(os.path.expanduser(folder_path))
 
 def is_exist(folder_path):
-	return os.path.exists(os.path.abspath(folder_path))
+	return os.path.exists(os.path.expanduser(folder_path))
 	
 def path_join(*paths):
 	return os.path.join(*paths)
 
 def list_dir(path):
-	return os.listdir(os.path.abspath(path))
+	return os.listdir(os.path.expanduser(path))
