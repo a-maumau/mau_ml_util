1c98568de45261b500dc8af41d3485a05f411605
a-maumau <maumaumaumaumaumaumaumaumaumau@gmail.com>
2018-10-29 00:08:41 +0900

    fix

--- utils/argument_util.py
+++ utils/argument_util.py
@@ -7,8 +7,7 @@ def read_arguments_from_yaml(args, config_path):
             config = yaml.load(f)
 
         for config_arg in config["arguments"]:
-            print(config_arg.items())
-            (key, val) = config_arg.items()
+            (key, val) = list(config_arg.items())
             if key in args:
                 args[key] = val
     except Exception as e:
