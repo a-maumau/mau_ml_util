6734faf77e8188cf3c1516bc2dfe60019f1c3c13
a-maumau <maumaumaumaumaumaumaumaumaumau@gmail.com>
2018-10-29 00:07:46 +0900

    fix

--- utils/argument_util.py
+++ utils/argument_util.py
@@ -7,6 +7,7 @@ def read_arguments_from_yaml(args, config_path):
             config = yaml.load(f)
 
         for config_arg in config["arguments"]:
+            print(config_arg.items())
             (key, val) = config_arg.items()
             if key in args:
                 args[key] = val
