72f377618a8dae041ef5f7e3283fe6a90303c326
a-maumau <maumaumaumaumaumaumaumaumaumau@gmail.com>
2018-10-28 23:41:53 +0900

    fix

--- metric/__init__.py
+++ metric/__init__.py
@@ -1,3 +1,4 @@
-from .segmentation import SegmentationMetric
+from .segmentation_metric import SegmentationMetric
+from .classification_metric import ClassificationMetric
 
 __all__ = ["SegmentationMetric"]
