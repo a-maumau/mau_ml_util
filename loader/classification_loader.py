cfd513779a95efcae8a68d29726b8b263217b440
a-maumau <maumaumaumaumaumaumaumaumaumau@gmail.com>
2018-10-28 23:29:41 +0900

    fix

--- loader/classification_loader.py
+++ loader/classification_loader.py
@@ -1,4 +1,4 @@
-from ..templates.template_data_loader import Template_ClassificationDatasetLoader
+from ..templates.data_loader_template import Template_ClassificationDatasetLoader
 
 class ClassificationDatasetLoader(Template_ClassificationDatasetLoader):
     def __init__(self, img_root, img_list_path, dataset_pickle_path=None,
