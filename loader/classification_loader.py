d8fa9ed8a8feb14b40ad66db5c036bd332d15661
a-maumau <maumaumaumaumaumaumaumaumaumau@gmail.com>
2018-10-29 20:18:30 +0900

    fix

--- loader/classification_loader.py
+++ loader/classification_loader.py
@@ -5,12 +5,10 @@ class ClassificationDatasetLoader(Template_ClassificationDatasetLoader):
                        input_transform=None,
                        load_all_in_ram=True, img_ext=".jpg", img_convert_type="RGB",
                        pickle_img_key="image", pickle_label_key="label",
-                       pickle_path_data=False, pickle_path_relative=False,
-                       return_original=False):
+                       pickle_path_data=False, pickle_path_relative=False):
 
         super(ClassificationDatasetLoader, self).__init__(img_root, img_list_path, dataset_pickle_path,
                                                           input_transform,
                                                           load_all_in_ram, img_ext, img_convert_type,
                                                           pickle_img_key, pickle_label_key,
-                                                          pickle_path_data, pickle_path_relative,
-                                                          return_original)
+                                                          pickle_path_data, pickle_path_relative)
